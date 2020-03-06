f885d1990389de4273841184f69cf92477d3d924
altruistcoder <rishabhaggarwal0101@gmail.com>
2020-03-07 04:07:23 +0530

    Project Code Updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import todos from './data'
+import ListTodos from './components/ListTodos'
 
 class App extends Component {
+  state = {
+    todos: todos
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <ListTodos todos = {this.state.todos}/>
       </div>
     );
   }
---
+++ src/data.js
@@ -0,0 +1,12 @@
+const todos = [
+    {
+        'id': 1,
+        'text': 'Learn how React works'
+    },
+    {
+        'id': 2,
+        'text': 'Complete the application'
+    }
+]
+
+export default todos;
